77a9769deb131074a1ac131b646aea5eec7979c4
parnesen <patrick.arnesen@gmail.com>
2015-10-02 13:12:47 -0700

    clean umolested chatserver

---
+++ ChatServer/ChatServer/ChastServer.cs
@@ -0,0 +1,17 @@
+﻿using Photon.SocketServer;
+
+public class ChatServer : ApplicationBase
+{
+    protected override PeerBase CreatePeer(InitRequest initRequest)
+    {
+        return new ChatPeer(initRequest.Protocol, initRequest.PhotonPeer);
+    }
+
+    protected override void Setup()
+    {
+    }
+
+    protected override void TearDown()
+    {
+    }
+}
---
+++ ChatServer/ChatServer/ChatPeer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+public class ChatPeer : PeerBase
+{
+    private static readonly object syncRoot = new object();
+
+    public ChatPeer(IRpcProtocol protocol, IPhotonPeer unmanagedPeer)
+        : base(protocol, unmanagedPeer)
+    {
+        lock (syncRoot)
+        {
+            BroadcastMessage += this.OnBroadcastMessage;
+        }
+    }
+
+    private static event Action<ChatPeer, EventData, SendParameters> BroadcastMessage;
+
+    protected override void OnDisconnect(DisconnectReason disconnectCode, string reasonDetail)
+    {
+        lock (syncRoot)
+        {
+            BroadcastMessage -= this.OnBroadcastMessage;
+        }
+    }
+
+    protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+    {
+        var @event = new EventData(1) { Parameters = operationRequest.Parameters };
+        lock (syncRoot)
+        {
+            BroadcastMessage(this, @event, sendParameters);
+        }
+
+        var response = new OperationResponse(operationRequest.OperationCode);
+        this.SendOperationResponse(response, sendParameters);
+    }
+
+    private void OnBroadcastMessage(ChatPeer peer, EventData @event, SendParameters sendParameters)
+    {
+        if (peer != this)
+        {
+            this.SendEvent(@event, sendParameters);
+        }
+    }
+}
